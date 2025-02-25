053b4111e5b55f3920870a06fae426472367c24d
gerard.gamiz <gerard.gamiz@auleseebe.upc.edu>
2025-02-25 09:45:00 +0100

    solucion problema 1

--- Problema1.R
+++ Problema1.R
@@ -1,1 +1,10 @@
 #Codigo para problema 1
+
+#primera y segunda pregunta
+Rsq<-0.1923 #coeficient de determinacio
+cor<-sqrt(Rsq) #coeficient de correlació
+
+m<-0.06576 #tercera pregunta, m=valor mercat/variacion de 1, por eso es la primera recta
+b<-11.79755
+m*647.1+b #prediccion de x=647.1, primera recta ya que x=actius
+
